de89f0962ac7474cf886a6dccffaf4127abaf6b1
guidojo <guidojo@users.noreply.github.com>
2018-11-09 14:26:59 +0100

    Update eisd

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { eisd } from "eisd";
 commander
     .usage("[options] <directories...>")
     .arguments("[options] <directories...>")
-    .option("-a, --async", "Execute yarn async across all folders, output will be a mess")
+    .option("-a, --async", "Execute npm i async across all folders, output will be a mess")
     .option(
         "-e, --allowErrors",
         "Allow errors (at default we stop when there is one). NOTE: always true when in async mode!"
@@ -14,4 +14,4 @@ commander
 const allowErrors = commander.allowErrors === true;
 const async = commander.async === true;
 
-eisd("npm i", commander.args, "", true, async);
+eisd("npm i", commander.args, allowErrors, async);
